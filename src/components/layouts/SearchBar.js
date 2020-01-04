f7c45f3c0b5d87fac2a5cca6cacf64983f722e95
Abdul-fatai <staticdev20046@gmail.com>
2020-01-04 14:17:48 +0100

    Get logs and preloader added

---
+++ src/components/layouts/SearchBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <nav style={{ marginBottom: '30px' }} className="blue">
+      <div className="nav-wrapper">
+        <div className="input-field">
+          <input id="search" type="search" required />
+          <label htmlFor="search" className="label-icon">
+            {' '}
+            <i className="material-icons">search</i>{' '}
+          </label>
+          <i className="material-icons">close</i>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default SearchBar;
